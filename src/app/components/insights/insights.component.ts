6c8a8c1b31fe920789621eed89e013fd4bd84105
Jamie Morris <maloric@hotmail.com>
2017-05-30 15:21:25 +0100

    Tweaked bar width and text

--- src/app/components/insights/insights.component.ts
+++ src/app/components/insights/insights.component.ts
@@ -8,6 +8,8 @@ import * as d3 from 'd3';
 })
 export class InsightsComponent implements OnChanges {
 
+  private stepWidth = 10;
+
   @ViewChild('chart')
   private chartContainer: ElementRef;
 
@@ -33,16 +35,32 @@ export class InsightsComponent implements OnChanges {
     bars.enter()
       .append('div')
       .attr('class', 'bar')
-      .style('width', (key) => {
-        return this.hashtags[key] * 10 + 'px';
-      })
-      .text(key => key);
+      .style('width', (key) => this.getBarWidth(key))
+      .text(key => this.getBarText(key));
 
     bars.exit().remove();
 
-    bars.transition().style('width', (key) => {
-      return this.hashtags[key] * 10 + 'px';
-    }).text(key => key);;
+    bars.transition()
+      .style('width', (key) => this.getBarWidth(key))
+      .text(key => this.getBarText(key));
+  }
+
+  getBarText(key): string {
+    return `#${key} (${this.hashtags[key]})`;
+  }
+
+  getBarWidth(key): string {
+    if (!this.hashtags) {
+      return '0';
+    }
+    let highest = this.hashtags[this.keys[0]] * this.stepWidth;
+
+    if (highest <= 100) {
+      return this.hashtags[key] * this.stepWidth + '%';
+    }
+    let width = (this.hashtags[key] / highest) * 100 * this.stepWidth;
+    // console.log(key, highest, this.hashtags[key], width);
+    return width + '%';
   }
 
 }
